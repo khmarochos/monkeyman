2eaef74b48cfad54938c50232f63f4dc343059bc
melnik13 <v.melnik@tucha13.net>
2014-06-30 17:45:54 +0300

    Added those nice help messages for a couple of tools

--- bin/mm_functions.sh
+++ bin/mm_functions.sh
@@ -5,7 +5,7 @@
 MM_PATH="/opt/monkeyman"
 MM_VMINFO="${MM_PATH}/bin/vminfo.pl"
 
-mm_vm_find() { "${MM_VMINFO}" --conditions "${@}" --short --short --xpath "/listvirtualmachinesresponse/virtualmachine/hostname/text()" --xpath "/listvirtualmachinesresponse/virtualmachine/instancename/text()"; }
+mm_vm_find()	{ "${MM_VMINFO}" --conditions "${@}" --short --short --xpath "/listvirtualmachinesresponse/virtualmachine/hostname/text()" --xpath "/listvirtualmachinesresponse/virtualmachine/instancename/text()"; }
 mm_vm_info()    { "${MM_VMINFO}" --conditions "${@}"; }
 mm_vm_whereis() { local A=($(mm_vm_find "${@}")); [ "${#A[@]}" -eq 2 ] && echo "The VM you're looking for is ${A[1]} at ${A[0]}"; }
 mm_vm_dumpxml() { local A=($(mm_vm_find "${@}")); [ "${#A[@]}" -eq 2 ] && ssh "${A[0]}" virsh dumpxml "${A[1]}"; }
--- lib/MonkeyMan/Show.pm
+++ lib/MonkeyMan/Show.pm
@@ -1,6 +1,8 @@
 package MonkeyMan::Show;
 
 use strict;
+use warnings;
+use feature "switch";
 
 use FindBin qw($Bin);
 
@@ -21,30 +23,67 @@ sub help {
 Usage: $0 [OPTIONS]
 
 GLOBAL OPTIONS:
-    -h,   --help          [O] Prints this message and exit
-          --version       [O] Prints only verion information and exit
+    -h,   --help
+        [opt] Prints this message and exit
+          --version
+        [opt] Prints only verion information and exit
     -c ?, --config ?      [O] Reads configuration from the specified file
-    -v,   --verbose       [O] Increasing verbosity
-    -q,   --quiet         [O] Silent mode
+        [opt] The main configuration file
+    -v,   --verbose
+        [opt] [mul] Increases verbosity
+    -q,   --quiet
+        [opt] [mul] Decreases verbosity
 
 __END_OF_GLOBAL_HELP_MESSAGE__
     );
 
-    if($mode eq 'runapicmd') {
-        print(<<__END_OF_RUNAPICMD_HELP_MESSAGE__
-MODULE ($mode) OPTIONS:
-    -p ?, --parameters ?  [M] The command and its parameters
-    -w ?, --wait ?        [O] To wait some time for an async job:
-                              if the number is omitted, I'll wait forever,
-                              but won't wait for anything by default
-    -x ?, --xpath ?       [O] To apply some XPath-queries
-    -s,   --short         [O] To get the result in a short form
+    given($mode) {
+        when('runapicmd') {
+            print(<<__END_OF_RUNAPICMD_HELP_MESSAGE__
 
+MODULE PURPOURSE:
 The purpourse of this module is to send commands to CloudStack's API and
 to show their results.
 
+MODULE ($mode) OPTIONS:
+    -p ?, --parameters ?
+        [req] [mul] The API command and its parameters
+    -w ?, --wait ?
+        [opt] To wait some seconds for an async job: if the number is
+        omitted, it will wait for the result as much as it's needed, but
+        it won't wait for the result bu default, jobid it to be returned
+    -x ?, --xpath ?
+        [opt] [mul] To apply some XPath-queries
+    -s,   --short
+        [opt] [mul] To get the result in a short form
+
 __END_OF_RUNAPICMD_HELP_MESSAGE__
-        );
+            );
+        }
+        when('vminfo') {
+            print(<<__END_OF_RUNAPICMD_HELP_MESSAGE__
+
+MODULE PURPOURSE:
+The purpourse of this module is to get information about virtual machines
+finding them by multiple criterias.
+
+MODULE ($mode) OPTIONS:
+    -o ?, --conditions ?
+        [opt] [mul] To find virtual machine(s) by certain conditions. You
+        can use anything you wish as a valid condition. Conditions are being
+        logically ANDed, so the expression
+            -o has_ipaddress=10.1.1.13 -o has_domain=CUSTOMER666
+        will find the virtual machine having the given IP-address belonging
+        to the given domain.
+    -x ?, --xpath ?
+        [opt] [mul] To apply some XPath-queries
+    -s,   --short
+        [opt] [mul] To get the result in a short form
+
+__END_OF_RUNAPICMD_HELP_MESSAGE__
+    
+
+        }
     }
 }
 
