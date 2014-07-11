8343df66a2025b6d8de9efb82c6415c5350e87e6
melnik13 <v.melnik@tucha13.net>
2014-07-11 16:50:34 +0300

    nou tajm tu ixplayn fak it

--- bin/makesnapshots.pl
+++ bin/makesnapshots.pl
@@ -94,7 +94,7 @@ THE_LOOP: while (1) {
 
         $log->debug("The schedule has been loaded");
 
-        # We shall forget all configuration elements as we obviously need
+        # We shall forget all configuration entities as we obviously need
         # to reload them all
 
         $configs = {};
@@ -104,20 +104,43 @@ THE_LOOP: while (1) {
 
     # Dealing with all configuration sections (reloading them if needed)
 
-    foreach my $type (qw/timeperiod storagepool host domain/) {
-        unless(keys(%{ $configs->{$type} })) {
-            $log->trace(mm_sprintify("Some %s definitely need to be defined", $type));
-            my $elements_loaded = eval {
-                load_elements(
-                    $schedule,
-                    $type,
-                    $configs->{$type}
-                );
-            };
-            $log->die(mm_sprintify("Can't load_element(): %s", $@))
-                if($@);
-            $log->trace(mm_sprintify("%d %ss have been loaded", $elements_loaded, $type));
+    foreach my $entity_type (qw/timeperiod storagepool host domain/) {
+
+        unless(keys(%{ $configs->{$entity_type} })) {
+            $log->trace(mm_sprintify("Some %s definitely need to be defined", $entity_type));
+
+            # Loading templates
+
+            foreach my $template_name (grep( /\*/, keys(%{ $schedule->{$entity_type} }))) {
+                $configs->{$entity_type}->{$template_name} = $schedule->{$entity_type}->{$template_name};
+                $log->trace(mm_sprintify("The %s %s's template has been loaded", $template_name, $entity_type));
+            }
+
+            # Loading entities
+
+            my $entities_loaded = 0;
+
+            foreach my $entity_name (grep(!/\*/, keys(%{ $schedule->{$entity_type} }))) {
+
+                $log->trace(mm_sprintify("Configuring the %s %s", $entity_name, $entity_type));
+
+                # Configuring the new entity, adding configuration templates
+         
+                my $layers_loaded = eval {
+                    configure_entity(
+                        $entity_type,
+                        $entity_name
+                    );
+                };
+                $log->logdie(mm_sprintify("Can't configure_entity(): %s", $@))
+                    if($@);
+                $entities_loaded++;
+
+            }
+
+            $log->trace(mm_sprintify("%d %ss have been loaded", $entities_loaded, $entity_type));
         }
+
     }
 
 
@@ -131,7 +154,7 @@ THE_LOOP: while (1) {
 
         # Reload the information about the domain only if it's needed
 
-        unless(defined($objects->{'domain'}->{'by_name'}->{$domain_path}->{'object'})) {
+        unless(defined($objects->{'domain'}->{'by_name'}->{$domain_path}->{'element'})) {
 
             $log->debug(mm_sprintify("Loading the information about the %s domain", $domain_path));
 
@@ -151,8 +174,8 @@ THE_LOOP: while (1) {
 
             $objects->{'domain'}->{'by_name'}->{$domain_path} =
             $objects->{'domain'}->{'by_id'}->{$domain_id} = {
-                object  => $domain,
-                used    => {}
+                element => $domain,
+                config  => $configs->{'domain'}->{$domain_path}
             };
 
             $log->info(mm_sprintify("The %s (%s) domain has been refreshed", $domain_id, $domain_path));
@@ -162,8 +185,12 @@ THE_LOOP: while (1) {
         # Do we need to scan for any downlinks?
 
         my $results = find_related_and_refresh_if_needed(
-            $objects->{'domain'}->{'by_name'}->{$domain_path}->{'object'},
-            $objects_relations->{'domain'}
+            $objects->{'domain'}->{'by_name'}->{$domain_path}->{'element'},
+            $objects_relations->{'domain'},
+            {
+                entity_name    => 'volume',
+                current_entity => undef
+            }
         );
         unless(defined($results)) {
             $log->warn(mm_sprintify("An error has occuried while refreshing %ss", $_));
@@ -178,35 +205,6 @@ THE_LOOP: while (1) {
 
     while(my($volume_id, $volume) = each(%{ $objects->{'volume'}->{'by_id'} })) {
 
-=pod
-        while(my($element_type, $element_features) = each({
-            domain      => {
-                required        => 1,
-                parameter       => 'domainid',
-                parameter_type  => 'id',
-                config          => \%conf_domains
-            }
-        }) {
-
-            my $element_id = $volume->{'object'}->get_parameter($element_features->{'parameter'});
-            if($volume->has_error) {
-                $log->warn($volume->{'object'}->error_message);
-                next;
-            } elsif($element_features->{'required'} && !defined($domain)) {
-                $log->warn(mm_sprintify("The %s volume doesn't have the %s parameter", %volume, $element_features->{'parameter'}));
-                next;
-            } else {
-
-                $element = $objects->{$element_type}->{"by_$element_features->{'parameter_type'}"}->{$domain};
-                unless(ref($domain->{'object'}) eq 'MonkeyMan::CloudStack::Elements::' . ${&MMElementsModule}{$element_type}) {
-                    $log->warn(mm_sprintify("The %s domain looks unhealthy", $domain));
-                    next;
-                }
-
-            }
-        }
-=cut
-
         # Adding the volume to the queue
 
         unless(defined($queue->{$volume_id})) {
@@ -216,24 +214,18 @@ THE_LOOP: while (1) {
                 postponed   => undef,
                 started     => undef,
                 done        => undef,
-                jobid       => undef,
-                related     => {}
+                jobid       => undef
             };
             $log->debug(mm_sprintify("Added the %s volume to the queue", $volume_id));
         }
 
-        # Updating the related objects' list
-
-=podV
-        $queue->{$volume_id}->{'related'} = {
-            domain          => $volume_domain,
-            storagepool     => $volume_storagepool,
-            virtualmachine  => $volume_virtualmachine,
-            host            => $virtualmachine_host
-        };
-=cut
-
     }
+
+
+    use Data::Dumper; $Data::Dumper::Indent = 2; $Data::Dumper::Terse = 1; $Data::Dumper::Maxdepth = 5;
+    $log->trace(Dumper($objects));
+    $log->trace(Dumper($configs));
+    last;
 
 
 
@@ -318,83 +310,13 @@ exit;
 
 
 
-sub load_elements {
-    
-    my($schedule, $elements_type, $elements_set) = @_;
+sub configure_entity {
+
+    my($entity_type, $entity_name) = @_;
 
     unless(
-        ref($schedule) eq 'HASH' &&
-        defined($elements_type) &&
-        ref($elements_set) eq 'HASH'
-    ) {
-        $log->logdie("Required parameters haven't been defined");
-    }
-
-    # Loading templates
-
-    foreach my $template_name (grep( /\*/, keys(%{ $schedule->{$elements_type} }))) {
-        $elements_set->{$template_name} = $schedule->{$elements_type}->{$template_name};
-        $log->trace(mm_sprintify("The %s %s's template has been loaded", $template_name, $elements_type));
-    }
-
-    # Loading elements
-
-    my $elements_loaded = 0;
-
-    foreach my $element_name (grep(!/\*/, keys(%{ $schedule->{$elements_type} }))) {
-
-        $log->trace(mm_sprintify("Configuring the %s %s", $element_name, $elements_type));
-
-        # Getting the configuration of the new element from the schedule
-        $elements_set->{$element_name} = $schedule->{$elements_type}->{$element_name};
-
-        # Configuring the new element, adding configuration templates
- 
-        my $element_configured = {};
-        my $layers_loaded = eval {
-            configure_element(
-                $elements_set,
-                $element_name,
-                $element_configured
-            );
-        };
-        $log->logdie(mm_sprintify("Can't configure_element(): %s", $@))
-            if($@);
-        $elements_set->{$element_name} = $element_configured;
-        $elements_loaded++;
-
-        $log->debug(mm_sprintify(
-            "The %s %s with %s configuration layers has been loaded",
-                $element_name,
-                $elements_type,
-                $layers_loaded
-        ));
-        foreach my $parameter (keys(%{ $elements_set->{$element_name} })) {
-            $log->trace(mm_sprintify(
-                "The %s %s has %s = %s",
-                    $element_name,
-                    $elements_type,
-                    $parameter,
-                    $elements_set->{$element_name}->{$parameter}
-            ));
-        }
-
-    }
-
-    return($elements_loaded);
-
-};
-
-
-
-sub configure_element {
-
-    my($elements_set, $element_name, $element_configured) = @_;
-
-    unless(
-        ref($elements_set) eq 'HASH' &&
-        defined($element_name) &&
-        ref($element_configured) eq 'HASH'
+        defined($entity_type) &&
+        defined($entity_name)
     ) {
         $log->logdie("Required parameters haven't been defined");
     }
@@ -405,20 +327,24 @@ sub configure_element {
 
     my $matched_patterns = 0;
 
-    foreach my $pattern (sort(keys(%{ $elements_set }))) {
-        if(match_glob($pattern, $element_name)) {
-            $log->trace(mm_sprintify("The %s pattern matched the %s element", $pattern, $element_name));
-            foreach (keys(%{ $elements_set->{$pattern} })) {
-                $element_configured->{$_} = $elements_set->{$pattern}->{$_};
-                $log->trace(mm_sprintify(
-                    "The %s element has %s = %s",
-                        $element_name,
-                        $_,
-                        $element_configured->{$_}
-                ));
+    foreach my $pattern (sort(keys(%{ $configs->{$entity_type} }))) {
+        if(match_glob($pattern, $entity_name)) {
+            $log->trace(mm_sprintify("The %s pattern matched the %s entity", $pattern, $entity_name));
+            foreach my $parameter (keys(%{ $configs->{$entity_type}->{$pattern} })) {
+                $configs->{$entity_type}->{$entity_name}->{$parameter} = $configs->{$entity_type}->{$pattern}->{$parameter};
             }
             $matched_patterns++;
         }
+    }
+
+    if($matched_patterns) {
+        $log->debug(mm_sprintify(
+            "The %s %s with %s configuration layers has been loaded: %s",
+                $entity_name,
+                $entity_type,
+                $matched_patterns,
+                $configs->{$entity_type}->{$entity_name}
+        ));
     }
 
     return($matched_patterns);
@@ -432,6 +358,7 @@ sub find_related_and_refresh_if_needed {
 
     my $uplink      = shift;
     my $uplink_node = shift;
+    my $key_entity = shift;
 
     unless(defined($uplink_node)) {
         $log->warn("The uplink's node isn't defined");
@@ -451,6 +378,14 @@ sub find_related_and_refresh_if_needed {
     if($uplink->has_error) {
         $log->warn($uplink->error_message);
         return;
+    }
+
+    # If the uplink is the key_entity, we need to store the link to it
+
+    if(
+        (defined($key_entity->{'entity_type'}) &&
+                ($key_entity->{'entity_type'} eq $uplink_type))) {
+        $key_entity->{'current_entity'} = $uplink_node;
     }
 
     # Do we need to scan for any downlinks?
@@ -545,13 +480,25 @@ sub find_related_and_refresh_if_needed {
                     next;
                 }
 
+                # Config/reconfig
+
+                unless(defined($configs->{$downlink_type}->{$downlink_name})) {
+                    my $layers_loaded = eval {
+                        configure_entity(
+                            $downlink_type,
+                            $downlink_name
+                        );
+                    };
+                    $log->logdie(mm_sprintify("Can't configure_entity(): %s", $@))
+                        if($@);
+                }
+
                 # Storing information about the downlink
 
                 $objects->{$downlink_type}->{'by_name'}->{$downlink_name} =
                 $objects->{$downlink_type}->{'by_id'}->{$downlink_id} = {
-                    object  => $downlink,
+                    element => $downlink,
                     config  => $configs->{$downlink_type}->{$downlink_name},
-                    used    => {}
                 };
 
                 $log->info(mm_sprintify(
@@ -563,10 +510,32 @@ sub find_related_and_refresh_if_needed {
 
             }
 
+            # Do we know the master?
+
+            if(
+                defined($key_entity->{'entity_type'}) &&
+                defined($key_entity->{'current_entity'})
+            ) {
+
+                # Setting object's master's entity reference to the correspondent master
+
+                $objects->{$downlink_type}->{'by_id'}->{$downlink_id}->{'masters'}->{ $key_entity->{'entity_type'} } =
+                    $key_entity->{'current_entity'};
+
+                # Setting master's reference to this object
+
+                push(
+                    @{$key_entity->{'current_entity'}->{'family'}->{$downlink_type}},
+                        $objects->{$downlink_type}->{'by_id'}->{$downlink_id}
+                );
+
+            }
+
+
             # Loading more downlinks it there are any
 
             my $results = find_related_and_refresh_if_needed(
-                $objects->{$downlink_type}->{'by_id'}->{$downlink_id}->{'object'},
+                $objects->{$downlink_type}->{'by_id'}->{$downlink_id}->{'element'},
                 $uplink_node->{$downlink_type}
             );
             unless(defined($results)) {
@@ -576,6 +545,14 @@ sub find_related_and_refresh_if_needed {
 
         }
 
+    }
+
+    # If the uplink was the key_entity, we must drop the link to it!
+
+    if(
+        (defined($key_entity->{'entity_name'}) &&
+                ($key_entity->{'entity_name'} eq $uplink_type))) {
+        $key_entity->{'current_entity'} = undef;
     }
 
     return($found);
