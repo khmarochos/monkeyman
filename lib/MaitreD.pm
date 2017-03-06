43a8e159d63fda00afb565029c406f3f6587d2cd
Vladimir Melnik <vladimir@melnik.net.ua>
2017-03-06 22:16:03 +0200

    34cebf83-73d9-4160-82fe-77fd8675ba1c

--- lib/MaitreD.pm
+++ lib/MaitreD.pm
@@ -28,42 +28,42 @@ method startup {
     $self->plugin('AssetManager', {
         assets_library => {
             js_pre      => {
-                jquery          => [ qw! /js/jquery-3.1.1.min.js                            ! ],
-                bootstrap       => [ qw! /js/bootstrap.min.js                               ! ],
-                i18next         => [ qw! /js/plugins/i18next/i18next.min.js                 ! ],
-                metismenu       => [ qw! /js/plugins/metisMenu/jquery.metisMenu.js          ! ],
-                slimscroll      => [ qw! /js/plugins/slimscroll/jquery.slimscroll.min.js    ! ]
+                jquery          => [ qw! /js/jquery-3.1.1.min.js                                ! ],
+                bootstrap       => [ qw! /js/bootstrap.min.js                                   ! ],
+                i18next         => [ qw! /js/plugins/i18next/i18next.min.js                     ! ],
+                metismenu       => [ qw! /js/plugins/metisMenu/jquery.metisMenu.js              ! ],
+                slimscroll      => [ qw! /js/plugins/slimscroll/jquery.slimscroll.min.js        ! ]
             },
             js          => {
-                multifields     => [ qw! /js/plugins/multiFields/multiFields.js             ! ],
-                steps           => [ qw! /js/plugins/steps/jquery.steps.min.js              ! ],
-                chosen          => [ qw! /js/plugins/chosen/chosen.jquery.js                ! ], #FIXME: to be removed
-                select2         => [ qw! /js/plugins/select2/select2.min.js                 ! ],
-                datatables      => [ qw! /js/plugins/dataTables/datatables.min.js           ! ],
-                toastr          => [ qw! /js/plugins/toastr/toastr.min.js                   ! ]
+                multifields     => [ qw! /js/plugins/multiFields/multiFields.js                 ! ],
+                steps           => [ qw! /js/plugins/steps/jquery.steps.min.js                  ! ],
+                select2         => [ qw! /js/plugins/select2/select2.min.js                     ! ],
+                datatables      => [ qw! /js/plugins/dataTables/datatables.min.js               ! ],
+                toastr          => [ qw! /js/plugins/toastr/toastr.min.js                       ! ],
+                googleMaps      => [ qw! https://maps.google.com/maps/api/js?key=AIzaSyC8eacjB7FIw6OH4OMCloaWeoRxiDdfJak&libraries=places ! ],
+                locationPicker  => [ qw! /js/plugins/locationPicker/locationpicker.jquery.js    ! ]
             },
             js_post     => {
-                inspinia        => [ qw! /js/inspinia.js                                    ! ],
-                pace            => [ qw! /js/plugins/pace/pace.min.js                       ! ]
+                inspinia        => [ qw! /js/inspinia.js                                        ! ],
+                pace            => [ qw! /js/plugins/pace/pace.min.js                           ! ]
             },
             css_pre     => {
-                bootstrap       => [ qw! /css/bootstrap.min.css                             ! ],
-                fontawesome     => [ qw! /font-awesome/css/font-awesome.css                 ! ]
+                bootstrap       => [ qw! /css/bootstrap.min.css                                 ! ],
+                fontawesome     => [ qw! /font-awesome/css/font-awesome.css                     ! ]
             },
             css         => {
-                toastr          => [ qw! /css/plugins/toastr/toastr.min.css                 ! ],
-                steps           => [ qw! /css/plugins/steps/jquery.steps.css                ! ],
-                chosen          => [ qw! /css/plugins/chosen/bootstrap-chosen.css           ! ], #FIXME: to be removed
-                select2         => [ qw! /css/plugins/select2/select2.min.css               ! ],
-                datatables      => [ qw! /css/plugins/dataTables/datatables.min.css         ! ],
-                datepicker      => [ qw! /css/plugins/datapicker/datepicker3.css            ! ],
+                toastr          => [ qw! /css/plugins/toastr/toastr.min.css                     ! ],
+                steps           => [ qw! /css/plugins/steps/jquery.steps.css                    ! ],
+                select2         => [ qw! /css/plugins/select2/select2.min.css                   ! ],
+                datatables      => [ qw! /css/plugins/dataTables/datatables.min.css             ! ],
+                datepicker      => [ qw! /css/plugins/datapicker/datepicker3.css                ! ],
                 summernote      => [ qw! /css/plugins/summernote/summernote.css
-                                         /css/plugins/summernote/summernote-bs3.css         ! ]
+                                         /css/plugins/summernote/summernote-bs3.css             ! ]
             },
             css_post    => {
-                animate         => [ qw! /css/animate.css                                   ! ],
-                style           => [ qw! /css/style.css                                     ! ],
-                maitre_d        => [ qw! /css/maitre-d.css                                  ! ]
+                animate         => [ qw! /css/animate.css                                       ! ],
+                style           => [ qw! /css/style.css                                         ! ],
+                maitre_d        => [ qw! /css/maitre-d.css                                      ! ]
             }
         }
     });
